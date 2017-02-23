fce476e0b26d042cc38a00ba33da98807f1f4ae1
Unbuensitio <info@unbuensitio.com>
2017-02-23 17:12:40 +0100

    Update statusbar.js

--- www/statusbar.js
+++ www/statusbar.js
@@ -12,6 +12,55 @@ StatusbarTransparent.prototype.enable = function (success, fail) {
 	}
 };
 
+StatusbarTransparent.prototype.ffffff = function (success, fail) {
+	if (this.transparentFlag == false) {
+		this.transparentFlag = true;
+		cordova.exec(success ? success : null, fail ? fail : null, 'StatusbarTransparent', 'enable', []);
+	}
+};
+StatusbarTransparent.prototype.000000 = function (success, fail) {
+	if (this.transparentFlag == false) {
+		this.transparentFlag = true;
+		cordova.exec(success ? success : null, fail ? fail : null, 'StatusbarTransparent', 'enable', []);
+	}
+};
+StatusbarTransparent.prototype.00796b = function (success, fail) {
+	if (this.transparentFlag == false) {
+		this.transparentFlag = true;
+		cordova.exec(success ? success : null, fail ? fail : null, 'StatusbarTransparent', 'enable', []);
+	}
+};
+StatusbarTransparent.prototype.0093DE = function (success, fail) {
+	if (this.transparentFlag == false) {
+		this.transparentFlag = true;
+		cordova.exec(success ? success : null, fail ? fail : null, 'StatusbarTransparent', 'enable', []);
+	}
+};
+StatusbarTransparent.prototype.C62F22 = function (success, fail) {
+	if (this.transparentFlag == false) {
+		this.transparentFlag = true;
+		cordova.exec(success ? success : null, fail ? fail : null, 'StatusbarTransparent', 'enable', []);
+	}
+};
+StatusbarTransparent.prototype.F8A724 = function (success, fail) {
+	if (this.transparentFlag == false) {
+		this.transparentFlag = true;
+		cordova.exec(success ? success : null, fail ? fail : null, 'StatusbarTransparent', 'enable', []);
+	}
+};
+StatusbarTransparent.prototype.627a86 = function (success, fail) {
+	if (this.transparentFlag == false) {
+		this.transparentFlag = true;
+		cordova.exec(success ? success : null, fail ? fail : null, 'StatusbarTransparent', 'enable', []);
+	}
+};
+StatusbarTransparent.prototype.4655AA = function (success, fail) {
+	if (this.transparentFlag == false) {
+		this.transparentFlag = true;
+		cordova.exec(success ? success : null, fail ? fail : null, 'StatusbarTransparent', 'enable', []);
+	}
+};
+
 /*
 * Change color statusbar
 */
